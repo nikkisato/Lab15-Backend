a6f4fca709a23f42eb7b0f861427baca85364414
Nikki <nikki.satopdx@gmail.com>
2020-01-05 19:53:30 -0800

    log out button

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,24 @@
 const root = document.getElementById('root');
+const form = document.createElement('form');
 
-const displayForm = () => {
-  const form = document.createElement('form');
+const displaySignup = () => {
+  form.id = 'Signup';
   form.innerHTML = `
-
-<fieldset>
-  <legend>Signup</legend>
-  Name: 
-  <input name="name" type="text" >
-  <br>
-  Email:<input name="email" type="text" >
-  <br>
-  Password:<input name="password" type="password" >
-</fieldset>
-<button href="button.html">Signup</button>
-`;
+    <fieldset>
+      <legend>Signup</legend>
+      <input name="name" type="text" placeholder="Name">
+      <input name="email" type="text" placeholder="Email">
+      <input name="password" type="password" placeholder="Password">
+      <button id="user-button">Signup</button>
+    </fieldset>
+    <p id="switcher">Already a user?</p>
+    <button id="display-login">Click here to log in!</button>
+  `;  
 
   form
     .addEventListener('submit', event => {
       event.preventDefault();
-
       const formData = new FormData(event.target);
-
       const user = {
         name: formData.get('name'),
         email: formData.get('email'),
@@ -42,13 +39,76 @@ const displayForm = () => {
     });
 
   root.appendChild(form);
+
+  const loginButton = document.getElementById('display-login');
+
+  loginButton
+    .addEventListener('click', displayLogin);
 };
 
-const displayUser = user => {
-  const h1 = document.createElement('h1');
-  h1.textContent = user.email;
+const displayLogin = () => {
+  form.id = 'Login';
+  form.innerHTML = `
+    <fieldset>
+      <legend>Login</legend>
+      <input name="email" type="text" placeholder="Email">
+      <input name="password" type="password" placeholder="Password">
+      <button id="user-button">Log In</button>
+    </fieldset>
+    <p id="switcher">Not yet a user?</p>
+    <button id="display-signup">Click here to sign up!</button>
+  `;
 
-  root.appendChild(h1);
+  form
+    .addEventListener('submit', event => {
+      event.preventDefault();
+
+      const formData = new FormData(event.target);
+      const user = {
+        email: formData.get('email'),
+        password: formData.get('password')
+      };
+
+      fetch('/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+        .then(res => res.json())
+        .then(() => {
+          window.location.reload();
+        });
+    });
+
+  root.appendChild(form);
+
+  const signupButton = document.getElementById('display-signup');
+
+  signupButton
+    .addEventListener('click', displaySignup);
+};
+
+const displayHomepage = user => {
+  const welcome = document.createElement('div');
+  welcome.innerHTML = `
+    <h1>Welcome ${user.name}!</h1>
+    <button id="logout">Log out</button>
+  `;
+
+  root.appendChild(welcome);
+
+  const deleteCookie = (name) => {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    window.location.reload();
+  };
+
+  const logoutButton = document.getElementById('logout');
+  logoutButton
+    .addEventListener('click', () => {
+      deleteCookie('session');
+    });
 };
 
 fetch('/api/v1/auth/verify', {
@@ -56,11 +116,10 @@ fetch('/api/v1/auth/verify', {
 })
   .then(res => res.json())
   .then(user => {
-    if(user._id) {
-      displayUser(user);
+    if(user._id){
+      window.location.href = './home.html';
     } else {
-      displayForm();
+      displayLogin();
     }
   });
 
-  
