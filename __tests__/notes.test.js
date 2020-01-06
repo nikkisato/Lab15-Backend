82bc8b2b787d8731a52a6243cc14f9bbef2626c5
Nikki <nikki.satopdx@gmail.com>
2020-01-05 20:52:56 -0800

    forgot the notes routes

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
@@ -85,30 +86,33 @@ describe('app routes', () => {
       });
   });
 
+
+  
+
   it('updates a note by id', async() => {
     await User.create({ 
-      email: 'corgi@corgi.com', 
+      email: 'update@corgi.com', 
       password: 'password' });
     
     const agent = request.agent(app);
 
     await agent
       .post('/api/v1/auth/login')
-      .send({ email: 'corgi@corgi.com', password: 'password' });
+      .send({ email: 'update@corgi.com', password: 'password' });
 
     return agent
-      .patch(`/api/v1/books/${note._id}`)
-      .send({ title: 'Portland' })
+      .patch(`/api/v1/notes/${note._id}`)
+      .send({ title: 'New York' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          title: 'Portland',
+          title: 'New York',
           text:'Sakura Blossom',
           __v: 0
         });
       });
-
   });
+
 
   it('deletes a note by id', async() => {
     await User.create({ 
